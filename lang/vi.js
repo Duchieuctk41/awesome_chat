97e54087ed5ffcc8fec0a71b4d57204ea46f8ae5
duchieu <duchieu.ctk41@gmail.com>
2020-08-22 19:27:27 +0700

    [1.0.0] Finish register user.

--- lang/vi.js
+++ lang/vi.js
@@ -4,3 +4,15 @@ gender_incorrect: "Bede phai hong tar??",
 password_incorrect: "Mat khau phai gom 8 ky tu, bao gom chu hoa, chu thuong, chu so va ky tu dac biet.",
 password_confirmation_incorrect: "Nhap lai mat khau chua chinh xac!"
 };
+
+export const transErrors = {
+    account_in_use: "user nay da duoc su dung.",
+    account_removed: "tai khoan nay da bi go khoi he thong, neu day la hieu lam, vui long lien he bo phan ho tro cua chung toi.",
+    account_not_active: "Email da dang ky nhung chua duoc Active tai khoan, vui long kiem tra email hoac lien he voi bo phan ho tro cua chung toi."
+};
+
+export const transSuccess = {
+    userCreated: (userEmail) => {
+        return `Tai khoan <strong>${userEmail} da duoc tao, kiem tra email de Active tai khoan truoc khi dang nhap, cam on</strong>`
+    }
+};
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import ConnectDB from "./config/connectDB";
 import configviewEngine from "./config/viewEngine";
 import initRoutes from "./routes/web";
 import bodyParser from "body-parser";
+import connectFlash from "connect-flash";
+import configSession from "./config/session";
 
 //Init app
 let app = express();
@@ -10,11 +12,17 @@ let app = express();
 //Connect to Mongodb
 ConnectDB();
 
+//Config session
+configSession(app);
+
 //Config view engine
 configviewEngine(app);
 
-//enable post data for request
+//Enable post data for request
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Enable flash messages
+app.use(connectFlash());
 
 //Init all routes
 initRoutes(app);
