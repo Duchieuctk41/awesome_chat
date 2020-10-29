20cfc82cf3b0d7ea1f8468995c08a5fa168c99b3
duchieu <duchieu.ctk41@gmail.com>
2020-10-29 19:00:48 +0700

    [1.0.0] read more messages.

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -364,10 +364,42 @@ let readMoreAllChat = (currentUserId, skipPersonal, skipGroup) => {
     });
 }
 
+/**
+ * 
+ * @param {string} currentUserId 
+ * @param {number} skipMessage 
+ * @param {string} targetId 
+ * @param {boolean} chatInGroup 
+ */
+let readMore = (currentUserId, skipMessage, targetId, chatInGroup) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            // message in group
+            if (chatInGroup) {
+                let getMessages = await MessageModel.model.readMoreMessagesInGroup(targetId, skipMessage, LIMIT_MESSAGES_TAKEN);
+                
+                getMessages = _.reverse(getMessages);
+
+                return resolve(getMessages);
+            } 
+
+            // message in personal
+            let getMessages = await MessageModel.model.readMoreMessagesInPersonal(currentUserId, targetId, skipMessage, LIMIT_MESSAGES_TAKEN);
+
+            getMessages = _.reverse(getMessages);  
+
+            return resolve(getMessages);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
 module.exports = {
     getAllConversationItems: getAllConversationItems,
     textAndEmojiChat: textAndEmojiChat,
     addNewImage: addNewImage,
     addNewAttachment: addNewAttachment,
-    readMoreAllChat: readMoreAllChat
+    readMoreAllChat: readMoreAllChat,
+    readMore: readMore
 };
